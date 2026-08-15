f847b7a252fbb0fdefa72b406d6fbe9b9c86929e
Alain Lima <alainlima88@gmail.com>
2026-08-15 17:10:00 +0200

    chore: add unit test target and shared scheme

---
+++ IPScannerTests/SmokeTests.swift
@@ -0,0 +1,15 @@
+//
+//  SmokeTests.swift
+//  IPScannerTests
+//
+//  Created by Alain Lima on 15/08/2026.
+//
+
+import XCTest
+@testable import IPScanner
+
+final class SmokeTests: XCTestCase {
+    func testProjectLoads() {
+        XCTAssertTrue(true)
+    }
+}
